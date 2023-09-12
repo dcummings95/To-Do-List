039c5386f62669f1bce3f545fd8bad471f3e9c77
Dominic Cummings <dominiccummings95@gmail.com>
2023-09-12 14:00:12 -0500

    Adding cross icon

--- script.js
+++ script.js
@@ -8,8 +8,17 @@ function addTask(){
         //Otherwise if they write something create a new list element and then made the innerHTML equal to the value user entered then add to list
     } 
     else {
+        //Create element li
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span");
+        //Add the cross icon to be on the end of the item
+        span.innerHTML = "\u00d7";
+        //Add it to the lsit
+        li.appendChild(span);
     }
+    //Reset the input box to be empty after user adds a task
+    inputBox.value = "";
+
 }
