def851530369248f78f7088ac6a7c17ad345e5ea
Dominic Cummings <dominiccummings95@gmail.com>
2023-09-12 14:09:29 -0500

    Saves data in browser

--- script.js
+++ script.js
@@ -20,15 +20,30 @@ function addTask(){
     }
     //Reset the input box to be empty after user adds a task
     inputBox.value = "";
+    //Save data whenever a task is added
+    saveData()
 }
 //add event listeners for the actions the user chooses
 listContainer.addEventListener("click", function(e){
     //If user clicks on the list element so whatever they had on their list then toggle checked so they can cross it out but keep it on
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        saveData()
     }
     //if user clicks on the x then remove the element from the list
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveData()
     }
 }, false);
+
+function saveData(){
+    //Save whatever data is inside of the list container into the browser
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function showTask(){
+    //Get all the data
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask()
