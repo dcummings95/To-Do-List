ee59b664df376ab014a50aa30a561fa712afecd6
Dominic Cummings <dominiccummings95@gmail.com>
2023-09-12 13:52:32 -0500

    addTask() created

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const inputBox = documeent.getElementById("input-box");
-const listContainer = documeent.getElementById("list-container");
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
 function addTask(){
-    
+    //If box is empty then let them know they need to add something
+    if(inputBox.value === ''){
+        alert("You must write something!");
+        //Otherwise if they write something create a new list element and then made the innerHTML equal to the value user entered then add to list
+    } 
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    }
 }
